f3482847d7b223bf021f7a5a6cecb24d87532b40
Amrita Mohanty <amritamohanty2004@gmail.com>
2013-11-25 01:03:39 -0800

    Fixed CustomerServices

--- src/goair/test/CustomerServicesTest.java
+++ src/goair/test/CustomerServicesTest.java
@@ -2,8 +2,13 @@ package goair.test;
 
 import java.rmi.RemoteException;
 import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import goair.model.customer.Customer;
+import goair.model.flight.Flight;
 import goair.model.reservation.Reservation;
+import goair.util.SearchParametersForFlights;
 import goair.util.SearchParametersForReservation;
 import goair.wsdl.CustomerServicesProxy;
 
@@ -29,9 +34,47 @@ public class CustomerServicesTest
 			e.printStackTrace();
 		}
 	}
+	
+	
+	public static void testValidateCustomer()
+	{
+		try {
+			Customer validCustomer = customerServicesProxy.customerLogin("a@gmail.com", "test");
+			if(validCustomer != null)
+			{
+				System.out.println("Valid customer : "+validCustomer.toString());
+			}
+		} catch (RemoteException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public static void testGetAllFlightsForCustomer()
+	{
+		try {
+			SearchParametersForFlights searchParametersForFlights = new 
+					SearchParametersForFlights();
+			searchParametersForFlights.setSource("Los Angeles");
+			searchParametersForFlights.setDestination("San Diego");
+			SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+			Calendar cal = Calendar.getInstance(); cal.setTime(dateFormat.parse("2013-11-04"));
+			searchParametersForFlights.setDateOfFlying(cal);
+			for(Flight flight : customerServicesProxy.searchFlights(searchParametersForFlights))
+			{
+				System.out.println("Flight : "+flight.toString());
+			}
+		} catch (RemoteException | ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) throws ParseException
 	{
-		CustomerServicesTest.testGetAllReservation();
+//		CustomerServicesTest.testGetAllReservation();
+		CustomerServicesTest.testValidateCustomer();
+		CustomerServicesTest.testGetAllFlightsForCustomer();
 		
 	}
 
